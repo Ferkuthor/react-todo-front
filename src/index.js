bafff5b5d2548c3604184bf4961b490434124963
Jonathan Cifuentes <cifuentes.jonathan@gmail.com>
2024-08-30 10:40:27 -0500

    One Single commit editado 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/app/app";
 
-ReactDOM.render(
-  <App />
-  ,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// 1 commit
+// 2 commit
+// 3 commit
+// 4 commit
+// 5 commit
+// 6 commit
+// 7 commit
+// 8 commit
